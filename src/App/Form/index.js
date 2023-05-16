ce40b7f45cd5cb1a761a71772c338a9f3811617b
Michal Maciejewski <michao931@gmail.com>
2023-05-17 00:06:12 +0200

    added-new-lines

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,44 +1,70 @@
-import "./style.css";
+import React, {useState} from "react";
+import {currencies} from "../currencies";
+import {Result} from "./Result";
+import "./style.css"
 
+export const Form = ({ calculateResult, result }) => {
+    const [currency, setCurrency] = useState(currencies[0].short);
+    const [amount, setAmount] =useState("");
 
-const Form = () => (
-    <div className="container">
-      <form className="form ">
-        <fieldset className="form__fieldset">
-          <legend className="form__legend">Kalkulator walut</legend>
-          <p>
-            <label>
-              <span className="form__labelText"> Kwota w zł*: </span>
-              <input
-             
-                placeholder="Wpisz kwotę"
-                name="name"
-                type="number"
-                className="form__field "
-                min="0"
-                step="any"
-                max="1000000"
-                required
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <span className="form__labelText"> Waluta: </span>
-              <select
-             
-              className="form__field " name="exchange">
-                <option value="EUR">EUR</option>
-                <option value="USD">USD</option>
-                <option value="CHF">CHF</option>
-              </select>
-            </label>
-          </p>
-          <button className="form__button">Przelicz</button>
-          <p className="">Wynik:</p>
-        </fieldset>
-      </form>
-    </div>
-)
+    const onSubmit = (event) => {
+        event.prevenDefault();
+        calculateResult(currency, amount);
+    }
 
-export default Form;
+    return (
+        <form className="form" onSubmit={onSubmit}>
+            <h1 className="form__header">
+                Przelicznik walut
+            </h1>
+            <p>
+                <label>
+                    <span className="form__labelText">
+                        Kwota w zł*:
+                    </span>
+                    <input
+                    value={amount}
+                    onChange={({ target }) => setAmount(target.value)}
+                    placeholder="Wpisz kwotę"
+                   type="number"
+                    className="form__field "
+                    min="0"
+                    step="any"
+                    max="1000000"
+                    required
+                    />
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">
+                        Waluta:
+                    </span>
+                    <select
+                    className="form__field"
+                    value={currency}
+                    onChange={({ target }) => setCurrency(target.value)}
+                    >
+                {currencies.map((currency => ( 
+                    <option
+                    key={currency.short}
+                    value={currency.short}
+                    >
+                {currency.name}
+                </option>
+                   )))}
+                </select>
+                </label>
+            </p>
+            <p>
+                <button className="form__button">Przelicz!</button>
+            </p>
+
+            <p className="form__info">
+                Kursy pochodzą ze strony nbp.pl
+            </p>
+
+            <Result result={result} />
+            </form>
+    )
+}
